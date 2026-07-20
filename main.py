8b070e03bbab6cd77f0d75085d2a2a5989a7cb92
john <johntravoltaengineer@gmail.com>
2026-07-20 22:54:48 +0300

    run first request thru openrouter API

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from playground!")
+import os
+import requests
 
+from dotenv import load_dotenv
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+response = requests.post(
+    url="https://openrouter.ai/api/v1/chat/completions",
+    headers={
+        "Authorization": f"Bearer {os.environ['OPENROUTER_API_KEY']}",
+    },
+    json={
+        "model": "nvidia/nemotron-3-ultra-550b-a55b:free",
+        "messages": [
+            {"role": "user", "content": "Hi"}
+        ],
+    },
+)
+
+#print(response.json())
+response.raise_for_status()
+
+print(response.json()["choices"][0]["message"]["content"])
